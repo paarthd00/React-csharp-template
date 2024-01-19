ac517119a8c11f37d703e9b857dcaa21192036f5
paarthd00 <paarthcode@gmail.com>
2024-01-18 18:45:51 -0800

    fix the routing error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -12,7 +11,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
